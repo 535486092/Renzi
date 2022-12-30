e13754ba5bba38632279f8d7e2e2e078b7a29a35
Mr.Guo <535486092@qq.com>
2022-12-30 14:52:29 +0800

    第一次提交

---
+++ src/settings.js
@@ -0,0 +1,17 @@
+module.exports = {
+  title: '人力资源管理系统',
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether fix the header
+   */
+  // 固定页面头部 ture固定 false不固定
+  fixedHeader: true,
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether show the logo in sidebar
+   */
+  // 左侧菜单顶部logo显示与隐藏  ture显示 false隐藏
+  sidebarLogo: true
+}
